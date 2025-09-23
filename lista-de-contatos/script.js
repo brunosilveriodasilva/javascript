f59c040d97c4bedb76e56593ff12190cbeefc492
brunosilveriodasilva <brunosilveriodasilva0@gmail.com>
2025-09-23 14:58:27 -0300

    atulizando cal

---
+++ lista-de-contatos/script.js
@@ -0,0 +1,16 @@
+const meuFormulario = document.querySelector("#meu-formulario");
+const inputNome = document.querySelector("#nome");
+const inputEmail = document.querySelector("#email");
+const mensagem = document.querySelector(".mensagem");
+const ususarios = document.querySelector("#usuarios");
+
+meuFormulario.addEventListener("submit", eviarFoumlario);
+
+function enviarFormulario(e) {
+    e.preventDefault();
+
+    
+    
+}
+
+
