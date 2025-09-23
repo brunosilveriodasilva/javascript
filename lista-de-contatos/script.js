9f3b91f18f0db327264d10eb4a5d8d3a7a537f7a
brunosilveriodasilva <brunosilveriodasilva0@gmail.com>
2025-09-23 16:03:32 -0300

    asdaw

--- lista-de-contatos/script.js
+++ lista-de-contatos/script.js
@@ -4,13 +4,30 @@ const inputEmail = document.querySelector("#email");
 const mensagem = document.querySelector(".mensagem");
 const ususarios = document.querySelector("#usuarios");
 
-meuFormulario.addEventListener("submit", eviarFoumlario);
+function reset() {
+    inputNome.value = "";
+    inputEmail.value = "";
+}
+
+meuFormulario.addEventListener("submit", enviarFormulario);
 
 function enviarFormulario(e) {
     e.preventDefault();
+    const usuarios = document.getElementById("usuarios");
+    usuarios.innerHTML = "";
+    
 
-    
-    
+    if (inputNome.value === "" && inputEmail.value === "") {
+        mensagem.style.color = "red";
+        mensagem.innerHTML = "Por favor, prencha o os campos abaxo";
+
+        setTimeout(() => mensagem.innerHTML = "", 2000);
+    } else if (inputNome.value.trim() !== "" && inputEmail.value.trim() !== "") {
+        mensagem.textContent = "";
+        usuarios.innerHTML = `
+            Seu nome escolhido foi: <mark>${inputNome.value}</mark><br>É seu Emalil é: <mark>${inputEmail.value}</mark>
+        `;
+    }
+
+    reset()
 }
-
-
